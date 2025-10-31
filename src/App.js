39186493106d72d5899851423e1939f6ca1bea25
labdhi <labdhi1705@gmail.com>
2025-10-31 19:42:12 +0000

    Basic start with portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import { Route, Routes ,Link } from "react-router-dom";
+import Home from './components/home/Home';
+import Project from './components/project/Project';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <Header />
+
+       
+       <Routes>
+       <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+       
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      
+      </>
+   
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,68 @@
+
+import React, { useState } from 'react'
+//import './.css'
+//import { Link, NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+import './header.css'
+
+const Header = () => {
+    const[isOpen, setIsOpen]= useState(false);
+const toggleMenu =()=>{
+    setIsOpen(!isOpen);
+}
+  return (
+   <>
+   <header>
+         <div className='container'>
+        <nav>
+        <div className='logo'>
+            <h2>Designer Studio</h2>
+        </div>
+        <div>
+            <ul className={isOpen ? "nav-link active" : "nav-link"}>
+                <li>
+                    <NavLink to="/" end className="active">
+                         Home
+                     </NavLink>
+                    
+                    </li>
+               <li>
+                <NavLink to="/project">
+                         Project
+                     </NavLink>
+                    </li>
+                
+               <li>
+                <NavLink to="/about">
+                         About
+                     </NavLink>
+                    </li>
+               
+               <li>
+                <NavLink to="/contact">
+                         Contact
+                     </NavLink>
+                    </li>
+                
+             
+            </ul>
+            <div className="icon" onClick={toggleMenu}>
+                <GiHamburgerMenu  />
+            </div>
+        </div>
+        </nav>
+         </div>
+    </header>
+    <section>
+        <div className="container">
+            <div className="content">
+                <h2>Responsive navbar</h2>
+            </div>
+        </div>
+    </section>
+      </>
+  )
+}
+
+export default Header
